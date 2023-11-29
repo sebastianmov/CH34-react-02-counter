478f6ac2877efb59b5ee7f8329bef62006b3ab5b
Sebastian Montes de Oca <ing.montesdeoca94@gmail.com>
2023-11-28 20:55:29 -0600

    add: Counter exercise

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  const increment = () => setCount(count + 1);
+  const decrement = () => setCount(count - 1);
+  const reset = () => setCount(0);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hola</h1>
+      {/* 
+      Agreguen un componente que tenga lo siguiente componente Cpunter
+      - Valor numerico 0
+      - 3 botones +, -, reset
+      */}
+      <p>Valor numerico: {count}</p>
+      <button onClick={increment}>+</button>
+      <button onClick={decrement}>-</button>
+      <button onClick={reset}>Reset</button>
     </div>
   );
 }
 
-export default App;
+export default Counter;
